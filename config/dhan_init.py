352ec454019d19bf0a0b3d9b4058a030c6480196
mdtamim <tamim.291@gmail.com>
2024-10-18 00:48:00 +0530

    Storing secrets in Google cloud

--- config/dhan_init.py
+++ config/dhan_init.py
@@ -1,19 +1,46 @@
 from dhanhq import dhanhq
-import os
+from google.cloud import secretmanager
 
-#TODO: Dont commit
-client_id = 'XXX'
-client_token = 'XXX'
+
+def get_client_token():
+    # Create a Secret Manager client
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Define the name of the secret
+    secret_name = "projects/dhan-algo/secrets/dhan-client-token/versions/latest"
+
+    # Access the secret version
+    response = client.access_secret_version(request={"name": secret_name})
+
+    # Get the token from the secret's payload
+    token = response.payload.data.decode("UTF-8")
+
+    return token
+
+def get_client_id():
+    # Create a Secret Manager client
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Define the name of the secret
+    secret_name = "projects/dhan-algo/secrets/dhan-client-id/versions/latest"
+
+    # Access the secret version
+    response = client.access_secret_version(request={"name": secret_name})
+
+    # Get the token from the secret's payload
+    client_id = response.payload.data.decode("UTF-8")
+
+    return client_id
 
 def init_dhan():
     # Initialize Dhan SDK
-    return dhanhq(client_id,client_token)
+    return dhanhq(get_client_id,get_client_token())
 
 url = "https://api.dhan.co/v2/marketfeed/ltp"
 
 headers = {
     "Accept": "application/json",
     "Content-Type": "application/json",
-    "access-token": client_token,
-    "client-id": client_id
+    "access-token": get_client_token(),
+    "client-id": get_client_id()
 }
--- config/firebase_init.py
+++ config/firebase_init.py
@@ -1,8 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from google.cloud import secretmanager
+import json
 
 def init_firebase():
-    cred = credentials.Certificate('C:\\Users\\user\\Documents\\dhan-algo-firebase-adminsdk-qg1gu-eb29f3fc8a.json')
+    client = secretmanager.SecretManagerServiceClient()
+    secret_name = "projects/dhan-algo/secrets/firebase-credentials/versions/latest"
+    response = client.access_secret_version(request={"name": secret_name})
+
+    # Load the credentials from the secret
+    credentials_data = response.payload.data.decode("UTF-8")
+    cred_dict = json.loads(credentials_data)
+
+    # Initialize Firebase Admin SDK
+    cred = credentials.Certificate(cred_dict)
+
     firebase_admin.initialize_app(cred)
     db = firestore.client()
     return db
